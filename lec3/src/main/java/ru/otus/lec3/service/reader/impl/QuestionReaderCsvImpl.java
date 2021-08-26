e80cdba5e5c14fb470e8a0afe4cbd48dc02f8342
Bondarchuk.dmitriy <dmitriy.bondarchuk@rt.ru>
2021-08-26 04:14:10 +0300

    Lec3 Правки в рамках PR2-2

--- lec3/src/main/java/ru/otus/lec3/service/reader/impl/QuestionReaderCsvImpl.java
+++ lec3/src/main/java/ru/otus/lec3/service/reader/impl/QuestionReaderCsvImpl.java
@@ -2,13 +2,15 @@ package ru.otus.lec3.service.reader.impl;
 
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
+import ru.otus.lec3.configuration.ReaderProperties;
 import ru.otus.lec3.model.Question;
 import ru.otus.lec3.service.exception.ReaderException;
 import ru.otus.lec3.service.reader.QuestionReader;
 
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.List;
@@ -16,16 +18,19 @@ import java.util.List;
 @Component
 public class QuestionReaderCsvImpl implements QuestionReader {
 
-    private final Resource resource;
+    private final ReaderProperties properties;
 
-    public QuestionReaderCsvImpl(
-            @Value("classpath:#{localizeServiceImpl.getLocalizeText('question.file')}") Resource resource) {
-        this.resource = resource;
+    public QuestionReaderCsvImpl(ReaderProperties properties) {
+        this.properties = properties;
     }
 
     @Override
     public List<Question> readQuestions() {
-        try (Reader reader = new InputStreamReader(resource.getInputStream());) {
+        Resource resource = new ClassPathResource(properties.getFilePathLocale());
+        if (!resource.exists()) {
+            resource = new ClassPathResource(properties.getFilePathDefault());
+        }
+        try (Reader reader = new InputStreamReader(resource.getInputStream())) {
             CsvToBean<Question> csvToBean = new CsvToBeanBuilder<Question>(reader)
                     .withType(Question.class)
                     .withIgnoreLeadingWhiteSpace(true)
--- lec3/src/test/java/ru/otus/lec3/reader/QuestionReaderTest.java
+++ lec3/src/test/java/ru/otus/lec3/reader/QuestionReaderTest.java
@@ -1,20 +1,35 @@
 package ru.otus.lec3.reader;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import ru.otus.lec3.configuration.ReaderProperties;
+import ru.otus.lec3.service.exception.ReaderException;
 import ru.otus.lec3.service.reader.QuestionReader;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class QuestionReaderTest {
 
     @Autowired
     private QuestionReader reader;
+
+    @MockBean
+    private ReaderProperties readerProperties;
+
+    @BeforeEach
+    void init() {
+        when(readerProperties.getFilePathLocale()).thenReturn("questions.csv");
+        when(readerProperties.getFilePathDefault()).thenReturn("questions.csv");
+    }
 
     @Test
     @DisplayName("Кол-во вопросов")
@@ -38,5 +53,13 @@ public class QuestionReaderTest {
     @DisplayName("Ответы не пустые")
     public void whenGetQuestionsGetAnswer_thenResultIsNotNull() {
         reader.readQuestions().forEach(question -> assertNotNull(question.getCorrectAnswer()));
+    }
+
+    @Test
+    @DisplayName("Ошибка чтения тестов")
+    public void itShouldThrowReaderExceptionWhenBadCsvFile() {
+        when(readerProperties.getFilePathLocale()).thenReturn("bad_questions.csv");
+        ReaderException th = assertThrows(ReaderException.class, reader::readQuestions);
+        assertEquals(th.getMessage(), "Ошибка: Не удалось прочитать тесты");
     }
 }
